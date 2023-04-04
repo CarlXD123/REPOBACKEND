fbf0e2d3fce8f4f45d9dd8afd408e456f08cbee6
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-04-04 02:44:32 -0500

    Update config.database.ts

--- src/config/config.database.ts
+++ src/config/config.database.ts
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize"
 
-const db = new Sequelize('SL_RedLab_Dev', 'postgres', '12345', {
-  host: 'localhost',
+const db = new Sequelize('railway', 'postgres', 'ySMDUp6glfA18TcbxPfP', {
+  host: 'containers-us-west-24.railway.app',
   dialect: 'postgres',
   logging: false,
-  port: 5432,
+  port: 6471,
   protocol: 'postgres',
   dialectOptions: {
       ssl: process.env.DB_ENABLE_SSL && {
