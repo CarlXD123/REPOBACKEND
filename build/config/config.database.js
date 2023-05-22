e6eb68f05cf57de83a75b7f5e894b44e2322748c
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-05-21 20:33:11 -0500

    Update config.database.js

--- build/config/config.database.js
+++ build/config/config.database.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('railway', 'postgres', 'ySMDUp6glfA18TcbxPfP', {
-    host: 'containers-us-west-24.railway.app',
+const db = new sequelize_1.Sequelize('railway', 'postgres', 'mRHbrGGtc2mJB56zMpca', {
+    host: 'containers-us-west-164.railway.app',
     dialect: 'postgres',
     logging: false,
     port: 6471,
